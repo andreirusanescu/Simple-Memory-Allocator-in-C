81f007f743969fad6c16f708aa27956c9293ffed
Andrei Rusanescu <andreirusanescu154@gmail.com>
2024-03-20 10:34:30 +0200

    Modificat functiile din biblioteca

--- func.c
+++ func.c
@@ -5,118 +5,195 @@
 
 typedef struct dll_node_t
 {
-    size_t address;
-	void* data;
+	void *data;
+	size_t address;
 	struct dll_node_t* next;
-    struct dll_node_t* prev;
+	struct dll_node_t* prev;
 } dll_node_t;
 
 typedef struct {
-    size_t address; // adresa headului practic;
-    dll_node_t* head;
+	size_t address; // adresa headului practic;
+	dll_node_t* head;
+	dll_node_t* tail;
 	unsigned int data_size; // marimea tipului de date din lista;
 	unsigned int size; // marimea listei;
 } doubly_linked_list_t;
 
 typedef struct {
-    int nlists;
-    int nbytes; // pe lista
-    int type_rec;
-    doubly_linked_list_t **list;
+	int nlists;
+	int nbytes; // pe lista
+	int type_rec;
+	doubly_linked_list_t **list;
 } SegregatedFreeLists;
 
 typedef struct {
-    int nlists;
-    int nbytes; // pe lista
-    doubly_linked_list_t **list;
+	int nlists;
+	doubly_linked_list_t **list;
 } OccupiedMemory;
 
 doubly_linked_list_t* dll_create(unsigned int data_size)
 {
-    doubly_linked_list_t *ll = malloc(sizeof(doubly_linked_list_t));
-    if (!ll) {
-        fprintf(stderr, "malloc() failed\n");
-        return NULL;
-    }
-    ll->head = NULL;
-    ll->data_size = data_size;
-    ll->size = 0;
-    return ll;
+	doubly_linked_list_t *ll = malloc(sizeof(doubly_linked_list_t));
+	if (!ll) {
+		fprintf(stderr, "malloc() failed\n");
+		return NULL;
+	}
+	ll->head = NULL;
+	ll->tail = NULL;
+	ll->data_size = data_size;
+	ll->size = 0;
+	return ll;
 }
 
 dll_node_t* dll_get_nth_node(doubly_linked_list_t* list, unsigned int n)
 {
-    if (!list || !list->head || n < 0)
-        return NULL;
-    n = n % list->size;
-    dll_node_t *node = list->head;
-    for (int i = 0; i < n; ++i)
-        node = node->next;
-    return node;
+	if (!list || !list->head || n < 0)
+		return NULL;
+	n = n % list->size;
+	dll_node_t *node = list->head;
+	for (int i = 0; i < n; ++i)
+		node = node->next;
+	return node;
 }
 
-void dll_add_nth_node(doubly_linked_list_t* list, unsigned int n, size_t address)
+// a fost scos parametrul void *data;
+void dll_add_nth_node(doubly_linked_list_t *list, unsigned int n, size_t address)
 {
-    if (!list || n < 0)
+	if (!list || n < 0)
         return;
     if (n > list->size)
         n = list->size;
-    if (n == 0) {
-        dll_node_t *node = malloc(sizeof(dll_node_t));
-        node->data = malloc(sizeof(list->data_size));
-        node->address = address;
-        node->prev = NULL;
-        node->next = list->head;
-        list->head = node;
-        list->size++;
-    } else {
-        dll_node_t *add = malloc(sizeof(dll_node_t));
-        add->data = malloc(sizeof(list->data_size));
-        dll_node_t *node = list->head;
-        for (int i = 0; i < n - 1; ++i)
-            node = node->next;
-        add->next = node->next;
-        add->prev = node;
-        node->next = add;
-        add->address = address;
-        list->size++;
-    }
+	dll_node_t *elem = malloc(sizeof(dll_node_t));
+	if (!elem) {
+		fprintf(stderr, "malloc() failed\n");
+		return;
+	}
+	// elem->data = data;
+	elem->address = address;
+	if (n == 0) {
+		if (!list->size) {
+			// lista e goala
+			list->head = elem;
+			list->tail = elem;
+			list->head->next = NULL;
+			list->tail->prev = NULL;
+			list->head->prev = NULL;
+			list->tail->next = NULL;
+			list->size++;
+		} else {
+			// am elemente in lista;
+			elem->next = list->head;
+			elem->prev = NULL;
+			list->head->prev = elem;
+			list->head = elem;
+			list->size++;
+		}
+	} else if (n == list->size) {
+		// adaug la final;
+		elem->next = NULL;
+		elem->prev = list->tail;
+		list->tail->next = elem;
+		list->tail = elem;
+		list->size++;
+	} else {
+		// adaug la mijloc
+		if (n > list->size / 2) {
+			// parcurg de la final;
+			dll_node_t* p = list->tail;
+			for (int i = list->size - 1; i >= n; --i)
+				p = p->prev;
+			elem->next = p;
+			elem->prev = p->prev;
+			p->prev->next = elem;
+			p->prev = elem;
+			list->size++;
+		} else {
+			// parcurg de la inceput;
+			dll_node_t* p = list->head;
+			for (int i = 0; i < n - 1; ++i)
+				p = p->next;
+			elem->prev = p;
+			elem->next = p->next;
+			p->next->prev = elem;
+			p->next = elem;
+			list->size++;
+		}
+	}
 }
 
-dll_node_t* dll_remove_nth_node(doubly_linked_list_t* list, unsigned int n)
+// sterge nodul de pe pozitia n si il intoarce
+dll_node_t* dll_remove_nth_node(doubly_linked_list_t *list, unsigned int n)
 {
-    if (!list || n < 0)
-        return NULL;
-    if (n == 0) {
-        dll_node_t *aux = list->head;
-        list->head = list->head->next;
-        if (list->head->next)
-            list->head->next->prev = NULL;
-        list->size--;
-        return aux;
-    } else {
-        if (n > list->size - 1)
-            n = list->size - 1;
-        dll_node_t *node = list->head;
-        for (int i = 0; i < n - 1; ++i)
-            node = node->next;
-        dll_node_t *aux = node->next;
-        aux->prev = node;
-        node->next = aux->next;
-        list->size--;
-        return aux;
-    }
+	if (!list || n < 0 || n > list->size || list->size == 0)
+		return NULL;
+	if (!n) {
+		// scot headul;
+		if (list->size == 1) {
+			// head = tail, practic sterg headul si tailul;
+			dll_node_t *aux = list->head;
+			list->head = NULL;
+			list->tail = NULL;
+			list->size--;
+			return aux;
+		} else {
+			// lista nu e goala;
+			dll_node_t* aux = list->head;
+			list->head = list->head->next;
+			list->head->prev = NULL;
+			list->size--;
+			return aux;
+		}
+	} else if (n == list->size - 1) {
+		// scot tailul;
+		if (list->size == 1) {
+			dll_node_t *aux = list->head;
+			list->head = NULL;
+			list->tail = NULL;
+			list->size--;
+			return aux;
+		} else {
+			dll_node_t *aux = list->tail;
+			list->tail = list->tail->prev;
+			list->tail->next = NULL;
+			list->size--;
+			return aux;
+		}
+	} else {
+		// scot de la mijloc;
+		if (n > list->size / 2) {
+			// parcurg de la final;
+			dll_node_t* p = list->tail;
+			for (int i = list->size - 1; i > n && p; --i)
+				p = p->prev;
+			dll_node_t *aux = p->prev;
+			p->prev = aux->prev;
+			aux->prev->next = p;
+			list->size--;
+			return aux;
+		} else {
+			// parcurg de la inceput;
+			dll_node_t* p = list->head;
+			for (int i = 0; i < n - 1 && p; ++i)
+				p = p->next;
+			dll_node_t* aux = p->next;
+			p->next = aux->next;
+			aux->next->prev = p;
+			list->size--;
+			return aux;
+		}
+	}
+	return NULL;
 }
 
-void dll_free(doubly_linked_list_t** pp_list)
+void dll_free(doubly_linked_list_t **list)
 {
-    if (!pp_list || !*pp_list)
-        return;
-    for (int i = 0; i < (*pp_list)->size; ++i) {
-        dll_node_t *aux = dll_remove_nth_node(*pp_list, 0);
-        free(aux->data);
-        free(aux);
-    }
-    free(*pp_list);
-    *pp_list = NULL;
+	if (!list || !*list) {
+		fprintf(stderr, "List is empty\n");
+		return;
+	}
+	while ((*list)->size) {
+		dll_node_t *elem = dll_remove_nth_node(*list, 0);
+		free(elem);
+	}
+	free(*list);
 }
--- tema1CA.c
+++ tema1CA.c
@@ -14,189 +14,187 @@ pt 1024 am 1 nod */
 
 void printaddress(SegregatedFreeLists *v)
 {
-    for (int i = 0; i < v->nlists; ++i) {
-        dll_node_t *node = v->list[i]->head;
-        for (int j = 0; j < v->list[i]->size && node; ++j) {
-            printf("%lx ", node->address);
-            node = node->next;
-        }
-        printf("\n");
-    }
+	for (int i = 0; i < v->nlists; ++i) {
+		dll_node_t *node = v->list[i]->head;
+		for (int j = 0; j < v->list[i]->size && node; ++j) {
+			printf("%lx ", node->address);
+			node = node->next;
+		}
+		printf("\n");
+	}
 }
 
 // merge bine functia asta
 void initheap(SegregatedFreeLists **v, OccupiedMemory **w,size_t heapbase, int nlists, int bytes, int type_rec)
 {
-    *v = (SegregatedFreeLists*)malloc(sizeof(SegregatedFreeLists));
-    if (!*v) {
-        fprintf(stderr, "malloc() failed\n");
-        return;
-    }
-    (*v)->list = (doubly_linked_list_t**)malloc(nlists * sizeof(doubly_linked_list_t*));
-    if (!(*v)->list) {
-        fprintf(stderr, "malloc() failed\n");
-        free(*v);
-        return;
-    }
+	*v = (SegregatedFreeLists*)malloc(sizeof(SegregatedFreeLists));
+	if (!*v) {
+		fprintf(stderr, "malloc() failed\n");
+		return;
+	}
+	(*v)->list = (doubly_linked_list_t**)malloc(nlists * sizeof(doubly_linked_list_t*));
+	if (!(*v)->list) {
+		fprintf(stderr, "malloc() failed\n");
+		free(*v);
+		return;
+	}
 
-    *w = (OccupiedMemory*)malloc(sizeof(OccupiedMemory));
-    if (!*w) {
-        fprintf(stderr, "malloc() failed\n");
-        free((*v)->list);
-        free(*v);
-        return;
-    }
-    (*w)->list = (doubly_linked_list_t**)malloc(nlists * sizeof(doubly_linked_list_t*));
-    if (!(*w)->list) {
-        fprintf(stderr, "malloc() failed\n");
-        free((*v)->list);
-        free(*v);
-        free(*w);
-        return;
-    }
-    size_t address = heapbase;
-    for (int i = 0; i < nlists; ++i) {
-        address = address + i * bytes;
-        (*v)->list[i] = dll_create(1 << (i + 3));
-        (*v)->list[i]->address = address;
-        (*w)->list[i] = dll_create(1 << (i + 3));
-        // (*w)->list[i]->data_size = -1; // asta inseamna ca e goala;
-        if (!(*v)->list[i]) {
-            fprintf(stderr, "malloc() failed\n");
-            for (int j = i - 1; j >= 0; --j) {
-                free((*v)->list[i]);
-            }
-            free((*v)->list);
-            return;
-        }
-        size_t node_address = address;
-        for (int j = 0; j < bytes / (1 << (i + 3)); ++j) {
-            // aloc gol;
-            node_address += j * bytes / (bytes / (1 << (i + 3)));
-            dll_add_nth_node((*v)->list[i], bytes / (1 << (i + 3)), node_address);
-        }
-    }
-    (*v)->nbytes = bytes;
-    (*v)->nlists = nlists;
-    (*v)->type_rec = type_rec;
+	// *w = (OccupiedMemory*)malloc(sizeof(OccupiedMemory));
+	// if (!*w) {
+	// 	fprintf(stderr, "malloc() failed\n");
+	// 	free((*v)->list);
+	// 	free(*v);
+	// 	return;
+	// }
+	// (*w)->list = (doubly_linked_list_t**)malloc(nlists * sizeof(doubly_linked_list_t*));
+	// if (!(*w)->list) {
+	// 	fprintf(stderr, "malloc() failed\n");
+	// 	free((*v)->list);
+	// 	free(*v);
+	// 	free(*w);
+	// 	return;
+	// }
+	size_t address = heapbase;
+	for (int i = 0; i < nlists; ++i) {
+		address = address + i * bytes;
+		(*v)->list[i] = dll_create(1 << (i + 3));
+		(*v)->list[i]->address = address;
+		// (*w)->list[i]->data_size = -1; // asta inseamna ca e goala;
+		if (!(*v)->list[i]) {
+			fprintf(stderr, "malloc() failed\n");
+			for (int j = i - 1; j >= 0; --j) {
+				free((*v)->list[i]);
+			}
+			free((*v)->list);
+			return;
+		}
+		size_t node_address = address;
+		for (int j = 0; j < bytes / (1 << (i + 3)); ++j) {
+			// aloc gol;
+			node_address += j * bytes / (bytes / (1 << (i + 3)));
+			dll_add_nth_node((*v)->list[i], bytes / (1 << (i + 3)), node_address);
+		}
+	}
+	(*v)->nbytes = bytes;
+	(*v)->nlists = nlists;
+	(*v)->type_rec = type_rec;
+	printaddress(*v);
 }
 
 // w are aceeasi structura cu segregated asta dar fac listele pe parcurs si dimensiunile o sa fie tot pe un rand, fix cat are primul nod din lista respectiva;
 void my_malloc(OccupiedMemory **w, SegregatedFreeLists **v, int bytes)
 {
-    if (!*v)
-        return;
-    int index = -1, ok = -1;
-    for (int i = 0; i < (*v)->nlists; ++i) {
-        if ((*v)->list[i]->data_size > bytes) {
-            if ((*v)->list[i]->head) {
-                ok = 1;
-                index = i;
-                break;
-            }
-        } else if ((*v)->list[i]->data_size == bytes) {
-            if ((*v)->list[i]->head) {
-                ok = 0;
-                index = i;
-                break;
-            }
-        }
-    }
-    if (index == -1) {
-        fprintf(stderr, "Out of memory\n");
-        return;
-    }
-    // am gasit indexul;
-    if (ok == 0) {
-        // nu fragmentez;
-        dll_node_t *node = dll_remove_nth_node((*v)->list[index], 0);
-        for (int i = 0; i < (*w)->nlists; ++i) {
-            // trebuie schimbat putin aici;
-            if ((*w)->list[i]->data_size == -1) {
-                (*w)->list[i]->data_size = (*v)->list[index]->data_size;
-                dll_add_nth_node((*w)->list[i], 0, node->address);
-            } else if ((*w)->list[i]->data_size == (*v)->list[index]->data_size) {
-                dll_add_nth_node((*w)->list[i], 0, node->address);
-            }
-        }
-        free(node->data);
-        free(node);
-    } else if (ok == 1) {
-        size_t dim_free = (*v)->list[index]->data_size - bytes;
-        // trebuie sa aloc un bloc de memorie de fix bytes, si unul de dim bytes si pe ala de dim bytes sa il bag in vectorul de liste unde trebuie;
-        // pe ala de dim bytes il adaug in lista mea
-        dll_node_t *node = dll_remove_nth_node((*v)->list[index], 0);
-        if (dim_free < 8) {
-            size_t rest_address = node->address + bytes;
-            for (int i = 0; i < (*v)->nlists; ++i) {
-                if ((*v)->list[i]->size == dim_free) {
-                    dll_add_nth_node((*v)->list[i], (*v)->list[i]->size, rest_address);
-                    break;
-                } else if ((*v)->list[i]->size > dim_free) {
-                    (*v)->list = (doubly_linked_list_t**)realloc((*v)->list, ((*v)->nlists + 1) * sizeof(doubly_linked_list_t*));
-                    if (!(*v)->list) {
-                        fprintf(stderr, "Realloc() failed\n");
-                        return;
-                    }
-                    (*v)->nlists++;
-                    (*v)->list[(*v)->nlists - 1] = dll_create(dim_free);
-                    (*v)->list[(*v)->nlists - 1]->address = rest_address;
-                    dll_add_nth_node((*v)->list[(*v)->nlists - 1], (*v)->list[(*v)->nlists - 1]->size, rest_address);
-                    for (int j = (*v)->nlists - 1; j >= 1; --j) {
-                        if ((*v)->list[j - 1]->size > (*v)->list[j]->size) {
-                            doubly_linked_list_t* aux = (*v)->list[j - 1];
-                            (*v)->list[j - 1] = (*v)->list[j];
-                            (*v)->list[j] = aux;
-                        }
-                    }
-                    break;
-                }
-            }
-        } else {
-            // fragmentez si mai mult;
-        }
-        for (int i = 0; i < (*w)->nlists; ++i) {
-            if ((*w)->list[i]->data_size == -1) {
-                (*w)->list[i]->data_size = (*w)->list[index]->data_size;
-                dll_add_nth_node((*w)->list[i], 0, node->address);
-            } else if ((*w)->list[i]->data_size == (*w)->list[index]->data_size) {
-                dll_add_nth_node((*w)->list[i], 0, node->address);
-            }
-        }
-        free(node->data);
-        free(node);
-    }
-    printaddress(*v);
+	if (!*v)
+		return;
+	int index = -1, ok = -1;
+	for (int i = 0; i < (*v)->nlists; ++i) {
+		if ((*v)->list[i]->data_size > bytes) {
+			if ((*v)->list[i]->head) {
+				ok = 1;
+				index = i;
+				break;
+			}
+		} else if ((*v)->list[i]->data_size == bytes) {
+			if ((*v)->list[i]->head) {
+				ok = 0;
+				index = i;
+				break;
+			}
+		}
+	}
+	if (index == -1) {
+		fprintf(stderr, "Out of memory\n");
+		return;
+	}
+	// am gasit indexul;
+	if (ok == 0) {
+		// nu fragmentez;
+		dll_node_t *node = dll_remove_nth_node((*v)->list[index], 0);
+		for (int i = 0; i < (*w)->nlists; ++i) {
+			// trebuie schimbat putin aici;
+			/* IMI ALOC ARRAYUL DE LISTE PT W SEPARAT SI DUPA VERIFIC DIN LISTA IN LISTA
+			DACA GASESC SI DACA NU DAU REALLOC SI MAI ADAUG O LISTA SI TOT ASA*/
+			// if ((*w)->list[i]->data_size == -1) {
+			// 	(*w)->list[i]->data_size = (*v)->list[index]->data_size;
+			// 	dll_add_nth_node((*w)->list[i], 0, node->address);
+			// } else if ((*w)->list[i]->data_size == (*v)->list[index]->data_size) {
+			// 	dll_add_nth_node((*w)->list[i], 0, node->address);
+			// }
+		}
+		free(node->data);
+		free(node);
+	} else if (ok == 1) {
+		size_t dim_free = (*v)->list[index]->data_size - bytes;
+		// trebuie sa aloc un bloc de memorie de fix bytes, si unul de dim bytes si pe ala de dim bytes sa il bag in vectorul de liste unde trebuie;
+		// pe ala de dim bytes il adaug in lista mea
+		dll_node_t *node = dll_remove_nth_node((*v)->list[index], 0);
+		size_t rest_address = node->address + bytes;
+		for (int i = 0; i < (*v)->nlists; ++i) {
+			if ((*v)->list[i]->size == dim_free) {
+				dll_add_nth_node((*v)->list[i], (*v)->list[i]->size, rest_address);
+				break;
+			} else if ((*v)->list[i]->size > dim_free) {
+				(*v)->list = (doubly_linked_list_t**)realloc((*v)->list, ((*v)->nlists + 1) * sizeof(doubly_linked_list_t*));
+				if (!(*v)->list) {
+					fprintf(stderr, "Realloc() failed\n");
+					return;
+				}
+				(*v)->nlists++;
+				(*v)->list[(*v)->nlists - 1] = dll_create(dim_free);
+				(*v)->list[(*v)->nlists - 1]->address = rest_address;
+				dll_add_nth_node((*v)->list[(*v)->nlists - 1], (*v)->list[(*v)->nlists - 1]->size, rest_address);
+				for (int j = (*v)->nlists - 1; j >= 1; --j) {
+					if ((*v)->list[j - 1]->size > (*v)->list[j]->size) {
+						doubly_linked_list_t* aux = (*v)->list[j - 1];
+						(*v)->list[j - 1] = (*v)->list[j];
+						(*v)->list[j] = aux;
+					}
+				}
+				break;
+			}
+		}
+		// adaugarea nodului in lista:
+		// for (int i = 0; i < (*w)->nlists; ++i) {
+		// 	if ((*w)->list[i]->data_size == -1) {
+		// 		(*w)->list[i]->data_size = (*w)->list[index]->data_size;
+		// 		dll_add_nth_node((*w)->list[i], 0, node->address);
+		// 	} else if ((*w)->list[i]->data_size == (*w)->list[index]->data_size) {
+		// 		dll_add_nth_node((*w)->list[i], 0, node->address);
+		// 	}
+		// }
+		free(node);
+	}
 }
 
 int main(void)
 {
-    char buffer[MAX_LEN];
-    SegregatedFreeLists *v = NULL;
-    OccupiedMemory *w = NULL;
-    int heapbase, nlists, nbytes, type_rec;
-    int ok = 1;
-    while (ok) {
-        scanf("%s", buffer);
-        if (!strcmp(buffer, "INIT_HEAP")) {
-            scanf("%x %d %d %d", &heapbase, &nlists, &nbytes, &type_rec);
-            initheap(&v, &w, heapbase, nlists, nbytes, type_rec);
-        } else if (!strcmp(buffer, "MALLOC")) {
-            scanf("%d", &nbytes);
-            my_malloc(&w, &v, nbytes);
-        } else if (!strcmp(buffer, "FREE")) {
-            
-        } else if (!strcmp(buffer, "READ")) {
-            
-        } else if (!strcmp(buffer, "WRITE")) {
-            
-        } else if (!strcmp(buffer, "DUMP_MEMORY")) {
-            
-        } else if (!strcmp(buffer, "DESTROY_HEAP")) {
-            // free
-            ok = 0;
-        }
-    }
+	char buffer[MAX_LEN];
+	SegregatedFreeLists *v = NULL;
+	OccupiedMemory *w = NULL;
+	int heapbase, nlists, nbytes, type_rec;
+	int ok = 1;
+	while (ok) {
+		scanf("%s", buffer);
+		if (!strcmp(buffer, "INIT_HEAP")) {
+			scanf("%x %d %d %d", &heapbase, &nlists, &nbytes, &type_rec);
+			initheap(&v, &w, heapbase, nlists, nbytes, type_rec);
+		} else if (!strcmp(buffer, "MALLOC")) {
+			scanf("%d", &nbytes);
+			my_malloc(&w, &v, nbytes);
+		} else if (!strcmp(buffer, "FREE")) {
+			scanf("%x", &heapbase);
+			// my_free(&w, heapbase);
+		} else if (!strcmp(buffer, "READ")) {
+			
+		} else if (!strcmp(buffer, "WRITE")) {
+			
+		} else if (!strcmp(buffer, "DUMP_MEMORY")) {
+			
+		} else if (!strcmp(buffer, "DESTROY_HEAP")) {
+			// free
+			ok = 0;
+		}
+	}
 
-    return 0;
+	return 0;
 }
